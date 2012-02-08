3f1c65c94bedf6b62e97dcbfd6c3b0f64baa690c
Jesse McCarthy <git_commits@jessemccarthy.net>
2012-02-08 09:41:48 -0500

    Added copyright & licensing statements and other comments.

--- Packages/Cycle Setting/Cycle Setting.py
+++ Packages/Cycle Setting/Cycle Setting.py
@@ -1,8 +1,23 @@
+"""
+
+Cycle Setting plugin for Sublime Text 2.
+Copyright 2011 Jesse McCarthy <http://jessemccarthy.net/>
+
+Adds a command that can be called from a key binding to toggle or
+cycle through values for a non-boolean setting.
+
+The Software may be used under the MIT (aka X11) license or Simplified
+BSD (aka FreeBSD) license.  See LICENSE
+
+"""
+
 import sublime, sublime_plugin
 
 class CycleSettingCommand( sublime_plugin.TextCommand ):
 
   def run( self, edit, setting, options ):
+
+    """Cycle $setting to next of $options"""
 
     options = list( set( options ) )
 
