41831b19dc031925dcc6bcb9b787596befb030c7
Jesse McCarthy <git_commits@jessemccarthy.net>
2012-02-23 09:13:42 -0500

    Release 1.0.0-2012-02-23-0913.

---
+++ Cycle Setting.py
@@ -0,0 +1,60 @@
+"""
+
+Cycle Setting plugin for Sublime Text 2.
+Copyright 2011 Jesse McCarthy <http://jessemccarthy.net/>
+
+Adds a command that can be called from a key binding to toggle or
+cycle through values for a non-boolean setting.
+
+The Software may be used under the MIT (aka X11) license or Simplified
+BSD (aka FreeBSD) license.  See LICENSE
+
+"""
+
+import sublime, sublime_plugin
+
+class CycleSettingCommand( sublime_plugin.TextCommand ):
+
+  def run( self, edit, setting, options ):
+
+    """Cycle $setting to next of $options"""
+
+    index = len( options )
+
+    if not index :
+
+      return
+
+
+    settings = self.view.settings()
+
+    value = settings.get( setting )
+
+
+    if value in options :
+
+      index = options.index( value )
+
+
+    index += 1
+
+
+    if index >= len( options ) :
+
+      index = 0
+
+
+    value = options[ index ]
+
+
+    settings.set( setting, value )
+
+
+    self.view.set_status(
+
+      'cycle_setting',
+
+      "Setting '{setting}' cycled to value '{value}'".format( **locals() )
+
+    )
+    
